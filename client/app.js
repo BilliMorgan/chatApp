6ec74fcc99468093cb27374e0137d9bec8eed7e2
Andrii Shymko <55424136+BilliMorgan@users.noreply.github.com>
2020-11-20 13:34:33 -0500

    relized chat functionality

--- client/app.js
+++ client/app.js
@@ -1,5 +1,16 @@
+var socket = io();
+
 $("form").on("submit", function () {
-  var text = $("#message").val();
-  alert(text);
+
+
+  var text =( $("#initials").val() + " says: " + $("#message").val());
+  socket.emit("message", text);
+  $("#message").val("");
+
   return false;
+});
+
+
+socket.on("message", function (msg) {
+  $("<li>").text(msg).appendTo("#history");
 });
