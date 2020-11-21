7cb63ddf4eeb8c53abb4618dfd552c2240e8af7f
Andrii Shymko <55424136+BilliMorgan@users.noreply.github.com>
2020-11-21 13:55:00 -0500

    deleted comments

--- client/app.js
+++ client/app.js
@@ -1,20 +1,13 @@
 var socket = io();
 
 $("form").on("submit", function () {
-
-  var text =( $("#initials").val() + " says: " + $("#message").val());
+  let text = $("#initials").val() + " says: " + $("#message").val();
   socket.emit("message", text);
   $("#message").val("");
   $("#initials").val("");
 
   return false;
 });
-
-
-// socket.on("connect", () => {
-//   // $("<li>").text().appendTo("#history");
-
-// });
 
 socket.on("message", function (msg) {
   $("<li>").text(msg).appendTo("#history");
--- server.js
+++ server.js
@@ -6,20 +6,17 @@ var server = http.Server(app);
 
 app.use(express.static("client"));
 var io = require("socket.io")(server);
-
-
-  let messageArhive = [];
+let messageArhive = [];
 
 io.on("connection", function (socket) {
-    messageArhive.map((message)=>{
-      socket.send(message);
-    })
+  messageArhive.map((message) => {
+    socket.send(message);
+  });
 
   socket.on("message", function (msg) {
-    messageArhive.push(msg)
+    messageArhive.push(msg);
     io.emit("message", msg);
   });
-  
 });
 
 let port = process.env.PORT;
@@ -28,9 +25,5 @@ if (port == null || port == "") {
 }
 
 server.listen(port, function () {
- console.log("Chat server running on port: 8080")
+  console.log("Chat server running on port: 8080");
 });
-
-// server.listen(8080, function () {
-//   console.log("Chat server running on port: 8080");
-// });
